8d25a534ccfc82b4b03452f43ca4abc46bd1b593
Olly Cohen <olly.k.cohen@gmail.com>
2020-03-01 21:16:35 -0600

    website framework

---
+++ about.php
@@ -0,0 +1,21 @@
+<?php require 'header.php'; ?> 
+
+
+    <div class="container">
+        <h1>About Us</h1>
+        <p>
+            Amo.Technology was founded in January 2020 by Andrew Mitra ‘21 and Olly Cohen ‘21 with the goal 
+            of building a bridge between Wash U computer science and the St. Louis technology industry. 
+        </p>
+        <p>
+            Andrew and Olly launched Developer Student Clubs (DSC) Wash U in August 2019, which currently has 
+            over 200 members and 7 projects building solutions for St. Louis nonprofits and businesses. 
+        </p>
+        
+        <p>
+            Based on their success starting DSC Wash U, Andrew and Olly received a grant to start their 
+            consulting group Amo.Technology. 
+        </p>
+    </div>
+
+</body>
