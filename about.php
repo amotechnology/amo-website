5021292fc5dae38b054f403dae1d1592fc663382
Olly Cohen <olly.k.cohen@gmail.com>
2020-03-01 22:31:43 -0600

    progress

--- about.php
+++ about.php
@@ -1,8 +1,13 @@
 <?php require 'header.php'; ?> 
 
+<link rel="stylesheet" href="css/about.css">
+</head>
 
+<body>
     <div class="container">
-        <h1>About Us</h1>
+        <div class="jumbotron">
+            <h1>About Us</h1>
+        </div>
         <p>
             Amo.Technology was founded in January 2020 by Andrew Mitra ‘21 and Olly Cohen ‘21 with the goal 
             of building a bridge between Wash U computer science and the St. Louis technology industry. 
@@ -11,11 +16,26 @@
             Andrew and Olly launched Developer Student Clubs (DSC) Wash U in August 2019, which currently has 
             over 200 members and 7 projects building solutions for St. Louis nonprofits and businesses. 
         </p>
-        
         <p>
             Based on their success starting DSC Wash U, Andrew and Olly received a grant to start their 
             consulting group Amo.Technology. 
         </p>
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <figure> 
+                        <img src="img/andrew.jpeg" alternate="Andrew Mitra, Washington University in St. Louis '21'">
+                        <figcaption id="acap"> Andrew Mitra, Washington University in St. Louis '21 </figcaption>
+                    </figure>
+                </div>
+                <div class="col-6"> 
+                    <img src="img/olly.jpg" alternate="Olly Cohen, Washington University in St. Louis '21'">
+                     <figcaption id="ocap"> Olly Cohen, Washington University in St. Louis '21 </figcaption>
+                </div>
+             </div>
+        </div>
+
     </div>
+
 
 </body>
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php require "header.php"; ?>
+
+</head>
+<body>
+<div>
+  <h1>Amo.Technology</h1>
+  <p>Amo.Technology is a Wash U-based consulting group that engages the most talented students in the computer science program to build technology solutions for St. Louis businesses.</p>
+</div>
+
+<div>
+  <h3>Why Amo.Technology consulting?</h3>
+  <p>We’re smart computer science students dedicated to solving your problems with technology. We’re here to help. We care about our clients and are committed to ensuring their success.</p>
+</div>
+
+<div>
+  <h3>Who we serve</h3>
+  <p>St. Louis Business: Empower local businesses with technology and promote the local tech industry by building a connection to Wash U students.</p>
+  <p>Students: Provide valuable real-world software engineering work to students during the semester that fit into their academic schedules and allow them to apply their skills from class.</p>
+</div>
+
+<div>
+  <h3>Social Impact:</h3>
+  <p>We also build pro-bono technology solutions for St. Louis nonprofits and socially driven businesses through our club Developer Student Clubs (DSC) Wash U.</p>
+</div>
+
+<div>
+  <h3>Our Principles</h3>
+  <p>Think big. Help. Love. Presence. Do the hard thing. Take it easy. People first. Beauty. Tell the truth. Open.
+</div>
+
+<div>
+  <h3>Our Process</h3>
+  <ul>
+    <li>Analyze - We make sure to completely understand the problem and think about the best way to solve it.</li>
+    <li>Design - We design a solution to the problem in an efficient and elegant manner.</li>
+    <li>Implement - We implement the solution by putting translating out design into code.</li>
+    <li>Reflect - We reflect on the work and see how it can be improved and the problems that are faced</li> 
+    <li>Repeat - We repeat the process.</li>
+  </ul>
+</div>
+</body>
+</html>
