fbf5bc8d6f0bedd59efeca06cbef09ac177b8805
Olly Cohen <olly.k.cohen@gmail.com>
2020-03-01 22:53:32 -0600

    homepage

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php require "header.php"; ?>
 
+<link rel="stylesheet" href="css/index.css">
 </head>
+
 <body>
+<div class="container">
 <div>
   <h1>Amo.Technology</h1>
-  <p>Amo.Technology is a Wash U-based consulting group that engages the most talented students in the computer science program to build technology solutions for St. Louis businesses.</p>
+  <p class="subhead">Amo.Technology engages talented computer science students at 
+  Washington University in St. Louis to build solutions for local businesses.</p>
 </div>
 
 <div>
-  <h3>Why Amo.Technology consulting?</h3>
+  <h3>Why Amo.Technology consulting</h3>
   <p>We’re smart computer science students dedicated to solving your problems with technology. We’re here to help. We care about our clients and are committed to ensuring their success.</p>
 </div>
 
@@ -19,24 +23,25 @@
 </div>
 
 <div>
-  <h3>Social Impact:</h3>
+  <h3>Passion</h3>
   <p>We also build pro-bono technology solutions for St. Louis nonprofits and socially driven businesses through our club Developer Student Clubs (DSC) Wash U.</p>
 </div>
 
 <div>
   <h3>Our Principles</h3>
-  <p>Think big. Help. Love. Presence. Do the hard thing. Take it easy. People first. Beauty. Tell the truth. Open.
+  <p>Think bigger. Help. Love. Presence. Do the hard thing. Take it easy. People first. Beauty. Tell the truth. Open.
 </div>
 
 <div>
   <h3>Our Process</h3>
   <ul>
-    <li>Analyze - We make sure to completely understand the problem and think about the best way to solve it.</li>
-    <li>Design - We design a solution to the problem in an efficient and elegant manner.</li>
-    <li>Implement - We implement the solution by putting translating out design into code.</li>
-    <li>Reflect - We reflect on the work and see how it can be improved and the problems that are faced</li> 
-    <li>Repeat - We repeat the process.</li>
+    <li>Listen - Understand our client.</li>
+    <li>Design - Solve the problem. </li>
+    <li>Build - Write the code. </li>
+    <li>Reflect - What did we get wrong? How can we improve? </li> 
+    <li>Iterate.</li>
   </ul>
 </div>
+<div>
 </body>
 </html>
