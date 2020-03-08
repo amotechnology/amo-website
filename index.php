bda3ce669752ec7d55a64c96878992f94e02037e
Andrew Mitra <andrew89876@ip-172-31-16-129.us-east-2.compute.internal>
2020-03-08 04:54:38 +0000

    temp changes

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
   <h3>Who we serve</h3>
   <p>St. Louis Business: Add value to local businesses with technology and strengthen the connection between St. Louis tech and Wash U students. </p>
   <a class="btn btn-primary btn-custom" href="https://docs.google.com/forms/d/e/1FAIpQLScmWtM68ttSFSujNlNqGmJ09FSPTa7CKpnaEGImf9pnDB8I2g/viewform" target="_blank" role="button">Submit Interest Form</a>
-  <br>
+  <br><br>
   <p>Students: Provide valuable real-world software engineering work during the semester that fits into their academic schedules. </p>
 <a class="btn btn-primary btn-custom" href="https://docs.google.com/forms/d/e/1FAIpQLSe24nCF1n-xwSImOXu5GiBat4MPV6f80QvljgU6iI-n_aM23A/viewform" target="_blank" role="button">Apply Here</a>
 </div>
