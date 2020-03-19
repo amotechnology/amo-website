3484403c1a12cb5757406754998ff49a2109566f
Olly Cohen <olly.k.cohen@gmail.com>
2020-03-18 19:00:24 -0500

    footer work

--- footer.php
+++ footer.php
@@ -2,18 +2,17 @@
 <div class="push"></div>
 <footer class="footer">
   <div>
-    <a class="footer-a" href="index.php">
-      Phone number: 973-856-1860
-      <br>
-      Address: 1 Brookings Drive, St. Louis MO 63105
-    </a>
+    <ul class="footer-list">
+        <li class="footer-item" id="email"><a href="mailto:amotechstl@gmail.com">amotechstl@gmail.com</a> </li>
+        <li class="footer-item" id="address">1 Brookings Drive, St. Louis MO 63105 </li>
+        <li class="footer-item" id="phone">973-856-1860 </li>
+    </ul>
   </div>
 
   
   <!-- <div>
     Social Links:
   </div><br><br> -->
-  <br><br>
 </footer>
 
 </body>
